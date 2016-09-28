e08279f16ab7fdc89ba3e088c2ff6979da4e91e5
edvox1138 <edvox1138@users.noreply.github.com>
2016-09-27 23:25:34 -0700

    Initial commit spring.io generated starter files

---
+++ src/main/java/com/edvox113/springboot/StartertoolApplication.java
@@ -0,0 +1,12 @@
+package com.edvox113.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartertoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartertoolApplication.class, args);
+	}
+}
